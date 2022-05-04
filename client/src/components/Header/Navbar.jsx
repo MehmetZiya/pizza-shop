b1f60f0a3f08071d76fac079f3b2b2bc185b644a
Mehmet Ziya Yazgan <mehmetziya82@gmail.com>
2022-05-04 13:53:04 +0200

    navbar created

--- client/src/components/Header/Navbar.jsx
+++ client/src/components/Header/Navbar.jsx
@@ -3,13 +3,18 @@ import logo from '../../assets/vp-logo.png'
 const Navbar = () => {
   return (
     <nav>
-      <NavLink to='/'>
-        <div className='headerLogo'>
-          <img src={logo} alt='vp-logo' />
-          <span className='brand1'>VALENTINO</span>
-          <span className='brand2'>PIZZERIA</span>
-        </div>
-      </NavLink>
+      <div className='menu'>
+        <NavLink to='/'>
+          <div className='headerLogo'>
+            <img src={logo} alt='vp-logo' />
+            <span className='brand1'>VALENTINO</span>
+            <span className='brand2'>PIZZERIA</span>
+          </div>
+        </NavLink>
+        <NavLink to='/login'>Login</NavLink>
+      </div>
+
+      <div className='mobilMenu'> </div>
     </nav>
   )
 }
